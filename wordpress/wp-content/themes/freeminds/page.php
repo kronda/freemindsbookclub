047926de90134b1ac9f26ca034185e6ef887acee
Kronda <ephany@gmail.com>
2014-05-08 12:58:42 -0700

    page quote html structure

--- wordpress/wp-content/themes/freeminds/page.php
+++ wordpress/wp-content/themes/freeminds/page.php
@@ -11,8 +11,12 @@
 add_action( 'genesis_loop', 'freeminds_page_quote');
 
 function freeminds_page_quote() {
-  echo '<h5>' . the_field('quote') . '</h5>';
-  echo '<span>' . the_field('quote_attribution') . '</span>';
+  echo '<h5 class="page-quote">'; 
+  echo the_field('quote');
+  echo '</h5>';
+  echo '<span class="quote-attr">'; 
+  echo the_field('quote_attribution');
+  echo '</span>';
 }
 
 genesis();
